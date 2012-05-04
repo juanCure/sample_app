6e0f598f97419b148e847ac183a8805bc0f8c97c
Juan Manuel Rodriguez <manuel2489@gmail.com>
2012-05-04 08:49:54 -0500

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #Mis modificaciones
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   #get "users/new"
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
 
 
 	#Adding a mapping for the root route
@@ -10,7 +10,8 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
-
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
