085e1bf4b7c24a2ac5d7e4b279418f4567c6e627
Juan Manuel Rodriguez <manuel2489@gmail.com>
2012-05-07 22:01:11 -0500

    Populate the table Products

--- app/controllers/dynamic_pages_products_controller.rb
+++ app/controllers/dynamic_pages_products_controller.rb
@@ -1,10 +1,13 @@
 class DynamicPagesProductsController < ApplicationController
   def smartphones
+	@smartphones = Product.select("name, price, model, description").where("category = 1")
   end
 
   def tablets
+	@tablets = Product.select("name, price, model, description").where("category = 2")
   end
 
   def accessories
+	@accessories = Product.select("name, price, model, description").where("category = 3")
   end
 end
