306917cf6049b6a1ab579d03f984a549b024ab0f
Juan Manuel Rodriguez <manuel2489@gmail.com>
2012-05-21 01:11:02 -0500

    Finalizado checkout_shopping

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :id, :updated_at, :created_at, :cart, :quantity
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
   
   def total_price
     product.price * quantity
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
