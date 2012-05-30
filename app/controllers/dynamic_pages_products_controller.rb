322e650ecf5212a285951220d0806504e4e96f94
Juan Manuel Rodriguez <manuel2489@gmail.com>
2012-05-29 22:49:38 -0500

    Ultimas modificacioes

--- app/controllers/dynamic_pages_products_controller.rb
+++ app/controllers/dynamic_pages_products_controller.rb
@@ -33,18 +33,18 @@ class DynamicPagesProductsController < ApplicationController
   end
 
   def accessories
-	#@accessories = Product.select("name, price, model, description").where("category = 3")
-	@accessories = Product.where("category = 4 OR category = 2").paginate(page: params[:page])
-	  if params[:mark]
-     @accessories = Product.where("category = 4 OR category = 2 AND mark = ?", params[:mark]).paginate(page: params[:page])
-   end
-   if params[:category]
-    @accessories = Product.where("category = ? ", params[:category]).paginate(page: params[:page])
-   end
-   if params[:start_price]
-    @accessories = Product.where("category = 4 OR category = 2 ", :price => (params[:start_price].to_i)..(params[:end_price].to_i))
-    .order("price ASC")
-    .paginate(page: params[:page])
-   end
+    #@accessories = Product.select("name, price, model, description").where("category = 3")
+    @accessories = Product.where("category = 4 OR category = 2").paginate(page: params[:page])
+    if params[:mark]
+      @accessories = Product.where("category = 4 OR category = 2 AND mark = ?", params[:mark]).paginate(page: params[:page])
+    end
+    if params[:category]
+      @accessories = Product.where("category = ? ", params[:category]).paginate(page: params[:page])
+    end
+    if params[:start_price]
+      @accessories = Product.where(:category => [2,4], :price => (params[:start_price].to_i)..(params[:end_price].to_i))
+      .order("price ASC")
+      .paginate(page: params[:page])
+    end
   end
 end
